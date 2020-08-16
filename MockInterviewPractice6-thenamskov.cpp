1e066d4b820f617f10ac67ab05c5a4b53f7dddfe
theNamskov <41073590+theNamskov@users.noreply.github.com>
2020-08-16 08:27:36 -0500

    chore: get_prime_numbers docstring

--- MockInterviewPractice6-thenamskov.cpp
+++ MockInterviewPractice6-thenamskov.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include  <queue>
 
+/**
+* @brief Gets all prime numbers ranging from 0 to a given boundary
+*
+* This function uses the sieve of Eratosthenes and the queue data structure.
+* An array of all integers from 0 to the given boundary is generated with an assumption that all a prime numbers.
+* Knowing that the integer 2 is the first prime number, all multiples of it from 0 to the given boundary are can-
+* celled out. Any integer not cancelled out is a prime and hence stored in a queue. The process is repeated for
+* all non-cancelled integers.
+* Time complexity: O(n)
+* Space complexity: O(n)
+*
+* @param boundary - long long
+*
+* @return prime_numbers - std::queue<long long>
+*/
 std::queue<long long> get_prime_numbers(long long boundary) {
     std::queue<long long> prime_numbers;
     int counter = 0;
